c4904f3dc0d0bab201a59f35ffc2bf1baa550deb
Layne <Layne@atkingdom.com>
2022-11-23 23:20:19 +0800

    Site updated: 2022-11-23 23:20:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/4a17b156.html","posts/dfb8a81f.html","posts/2ea8de7b.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/dfb8a81f.html","posts/4a17b156.html","posts/2ea8de7b.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
