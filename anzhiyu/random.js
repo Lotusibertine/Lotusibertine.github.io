b131f239777ec27967deb16f212aff5be231e261
Layne <Layne@atkingdom.com>
2023-02-27 23:09:05 +0800

    Site updated: 2023-02-27 23:09:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/140ab620.html","posts/2ea8de7b.html","posts/7342d5b5.html","posts/8df660ab.html","posts/6c648fec.html","posts/dfb8a81f.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/140ab620.html","posts/7342d5b5.html","posts/2ea8de7b.html","posts/dfb8a81f.html","posts/8df660ab.html","posts/6c648fec.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
